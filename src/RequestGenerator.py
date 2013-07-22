6bb937675cceb41e6b5ef01e84a40bbb75380167
scubeWasTaken <sagar.sugandhi@gmail.com>
2013-07-22 09:56:46 -0400

    indents

--- src/RequestGenerator.py
+++ src/RequestGenerator.py
@@ -3,53 +3,53 @@ from selenium import webdriver
 import time
 
 class RequestGenerator:
-	fp = None
-	
-	def __init__(self, profile=Utils.getConfig("profile", "firefox")):
-		#1. Add HTTP-Request Logger extension to Firefox
-		#2. Set Firefox profile so that HTTP-Request Logger will be available
-		self.fp = webdriver.FirefoxProfile(profile)
+    fp = None
+    
+    def __init__(self, profile=Utils.getConfig("profile", "firefox")):
+        #1. Add HTTP-Request Logger extension to Firefox
+        #2. Set Firefox profile so that HTTP-Request Logger will be available
+        self.fp = webdriver.FirefoxProfile(profile)
 
-	#start generation of HTTP Request for resources on supplied webpage
-	def start(self, URLlist):
-		#code to start the browser
-		browser = webdriver.Firefox(self.fp)
-		browser.set_page_load_timeout(5)
-		#Each URL has a precalculated link set associated with it which contains URL's.
-		
-		for url in URLlist:
-			try:
-   				with open("../data/requests/" + url.rstrip() + '.request_raw'): 
-					#Website already processed.
-					continue
-			except IOError:
-				print 'Error while opening the raw request file. Regenerating request.'
+    #start generation of HTTP Request for resources on supplied webpage
+    def start(self, URLlist):
+        #code to start the browser
+        browser = webdriver.Firefox(self.fp)
+        browser.set_page_load_timeout(5)
+        #Each URL has a precalculated link set associated with it which contains URL's.
+        
+        for url in URLlist:
+            try:
+                with open("../data/requests/" + url.rstrip() + '.request_raw'): 
+                    #Website already processed.
+                    continue
+            except IOError:
+                print 'Error while opening the raw request file. Regenerating request.'
 
-			linkSetFileName = "../data/linkset/" + url.rstrip() + ".linkset"
-			
-			linkSet = Utils.readFile(linkSetFileName)
-			for link in linkSet:
-				try:
-					if link.find("http")<0:
-						browser.get("http://" + link) # Load page
-					else:
-						browser.get(link)
-				except:
-					#print "error while processing " + link
-					continue
+            linkSetFileName = "../data/linkset/" + url.rstrip() + ".linkset"
+            
+            linkSet = Utils.readFile(linkSetFileName)
+            for link in linkSet:
+                try:
+                    if link.find("http")<0:
+                        browser.get("http://" + link) # Load page
+                    else:
+                        browser.get(link)
+                except:
+                    #print "error while processing " + link
+                    continue
 
-				time.sleep(0.5)
+                time.sleep(0.5)
 
-			while True:
-				try:
-					browser.get("")
-					break
-				except:
-					time.sleep(0.3)
-					continue
+            while True:
+                try:
+                    browser.get("")
+                    break
+                except:
+                    time.sleep(0.3)
+                    continue
 
-			time.sleep(0.3)
-			Utils.execScript("bash_script_move.sh", url)
+            time.sleep(0.3)
+            Utils.execScript("bash_script_move.sh", url)
 
-			time.sleep(0.5)
-		browser.close()
+            time.sleep(0.5)
+        browser.close()
--- src/ResponseGenerator.py
+++ src/ResponseGenerator.py
@@ -15,169 +15,169 @@ from ResourceDetails import ResourceDetails
 
 
 class HeadRequest(urllib2.Request):
-	def get_method(self):
-		return "GET"
+    def get_method(self):
+        return "GET"
 
 class HTTPReqRes:
 
-	def __init__(self, URL):
-		self.URL = URL
-		self.requestFileRaw = Utils.getConfig("requestFolder") + URL.rstrip() + ".request_raw"
-		self.requestFile = Utils.getConfig("requestFolder") + URL.rstrip() + ".request"
-		self.responseFile = Utils.getConfig("responseFolder") + URL.rstrip() + ".response"
-		self.completeResponseStringForAllLinks = ""
+    def __init__(self, URL):
+        self.URL = URL
+        self.requestFileRaw = Utils.getConfig("requestFolder") + URL.rstrip() + ".request_raw"
+        self.requestFile = Utils.getConfig("requestFolder") + URL.rstrip() + ".request"
+        self.responseFile = Utils.getConfig("responseFolder") + URL.rstrip() + ".response"
+        self.completeResponseStringForAllLinks = ""
 
-	#Removes referrer, GET keyword and also removes redundant requests and creates a new file 
-	def normalizeRequestList(self):
-		#print "\nIn Normalize request\n"
-		uniqueURLs = Set()
-		try:
-			with open(self.requestFileRaw) as f:
-				for line in f:
-					breakflg=0;
-					arr = line.split(" ")
-					for val in arr:
-						if breakflg == 1:
-							uniqueURLs.add(val)
+    #Removes referrer, GET keyword and also removes redundant requests and creates a new file 
+    def normalizeRequestList(self):
+        #print "\nIn Normalize request\n"
+        uniqueURLs = Set()
+        try:
+            with open(self.requestFileRaw) as f:
+                for line in f:
+                    breakflg=0;
+                    arr = line.split(" ")
+                    for val in arr:
+                        if breakflg == 1:
+                            uniqueURLs.add(val)
 
-	#RFC 2616 states that Some HTTP methods MUST cause a cache to invalidate an entity. This is either the entity referred to by the Request-URI, or by the Location or Content-Location headers (if present). These methods are:
+    #RFC 2616 states that Some HTTP methods MUST cause a cache to invalidate an entity. This is either the entity referred to by the Request-URI, or by the Location or Content-Location headers (if present). These methods are:
 
-	#  - PUT
-	#  - DELETE
-	#  - POST
+    #  - PUT
+    #  - DELETE
+    #  - POST
 
-	#Hence only considered GET
-						if val == "GET":
-							breakflg=1;
-			request_fd = open(self.requestFile,"w+")
-			request_fd.write("".join(uniqueURLs))
-			request_fd.close()
-		except:
-			print "Error in normalize request"
+    #Hence only considered GET
+                        if val == "GET":
+                            breakflg=1;
+            request_fd = open(self.requestFile,"w+")
+            request_fd.write("".join(uniqueURLs))
+            request_fd.close()
+        except:
+            print "Error in normalize request"
 
-	#Form request, get response and process Response
-	def handleRequest(self):
-		#1. Form request object
-		#2. Get response
-		#3. Process and print response header
-		#print "\nIn handle request"
-		print "PROCESSING " + self.URL.rstrip() + " START"
+    #Form request, get response and process Response
+    def handleRequest(self):
+        #1. Form request object
+        #2. Get response
+        #3. Process and print response header
+        #print "\nIn handle request"
+        print "PROCESSING " + self.URL.rstrip() + " START"
 
-		sitedetail = SiteDetails(self.URL.rstrip())
+        sitedetail = SiteDetails(self.URL.rstrip())
 
-		cacheableObjects = 0
-		totalObjects = 0
+        cacheableObjects = 0
+        totalObjects = 0
 
-		mynumbers = []
-		print self.requestFile
-		with open(self.requestFile) as f:
-			
-			for val in f:
-				rd = ResourceDetails(self.URL.rstrip())
-				rd.url = val.strip()
-		
-				#CHECK IS THE URL IS HTTPS...
-				if val.find('https') == 0:
-					rd.isHTTPS = 1
+        mynumbers = []
+        print self.requestFile
+        with open(self.requestFile) as f:
+            
+            for val in f:
+                rd = ResourceDetails(self.URL.rstrip())
+                rd.url = val.strip()
+        
+                #CHECK IS THE URL IS HTTPS...
+                if val.find('https') == 0:
+                    rd.isHTTPS = 1
 
-				#request = urllib2.Request(val, unverifiable=True)
-				request = HeadRequest(val)
-				try:
-					response = urllib2.urlopen(request, timeout=2)
-				except urllib2.URLError, e:
-					if isinstance(e.reason, socket.timeout):
-						print("%s -> There was an timeout error: %r %s" % (self.URL.rstrip(), e, val))
-					#else:
-						#print "Error in URL: " + val #+ " Reason: " + str(e.reason)
-					continue
-				except urllib2.HTTPError, e:
-					#print "Error in processing HTTP for URL: " + val #+ " Reason: " + str(e.reason)
-					continue
-				except:
-					#print "Unknown error in URL: " + val
-					continue
+                #request = urllib2.Request(val, unverifiable=True)
+                request = HeadRequest(val)
+                try:
+                    response = urllib2.urlopen(request, timeout=2)
+                except urllib2.URLError, e:
+                    if isinstance(e.reason, socket.timeout):
+                        print("%s -> There was an timeout error: %r %s" % (self.URL.rstrip(), e, val))
+                    #else:
+                        #print "Error in URL: " + val #+ " Reason: " + str(e.reason)
+                    continue
+                except urllib2.HTTPError, e:
+                    #print "Error in processing HTTP for URL: " + val #+ " Reason: " + str(e.reason)
+                    continue
+                except:
+                    #print "Unknown error in URL: " + val
+                    continue
 
-				rd.receivedDate = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+                rd.receivedDate = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-				code = response.getcode()
-				
-				if code in [500,501,502,503,504,505]:
-					continue
-				if code in [400,401,402,403,404,405,406,407,408,409,411,412,413,414,415,416,417]:
-					continue
-				if code in [303,304,305]:
-					continue
+                code = response.getcode()
+                
+                if code in [500,501,502,503,504,505]:
+                    continue
+                if code in [400,401,402,403,404,405,406,407,408,409,411,412,413,414,415,416,417]:
+                    continue
+                if code in [303,304,305]:
+                    continue
 
-				keys = response.info().items()
+                keys = response.info().items()
 
-				d = defaultdict(list)
-				for k,v in keys:
-					d[k].append(v)
+                d = defaultdict(list)
+                for k,v in keys:
+                    d[k].append(v)
 
-				try:
-					size = str(response.read().__sizeof__())
-				except:
-					if "".join(d['transfer-encoding']) != "":
-						size='N/A'
-					elif "".join(d['content-length']) == "":
-						size='N/A'
-					else:
-						size="".join(d['content-length'])
+                try:
+                    size = str(response.read().__sizeof__())
+                except:
+                    if "".join(d['transfer-encoding']) != "":
+                        size='N/A'
+                    elif "".join(d['content-length']) == "":
+                        size='N/A'
+                    else:
+                        size="".join(d['content-length'])
 
 
-				rd.contentLength = size
-				rd.statusCode = str(code)
-				rd.contentType = ",".join(d['content-type'])
-	
-				#Age is used to determine if the item in cache is fresh or not. So, it for post processing. It does not determin while storing that the object is cacheable or not.
-				#age 		= "".join(d['age']).strip()
-				
-				rd.lastModified 	= "".join(d['last-modified']).strip()
-				rd.expires 	= "".join(d['expires']).strip()
-				rd.pragma		= "".join(d['pragma']).strip()
-			
-			
-				#_______________________________________________________________________
-				cacheControl = ",".join(d['cache-control'])
-				cacheControlArray = cacheControl.split(",")
+                rd.contentLength = size
+                rd.statusCode = str(code)
+                rd.contentType = ",".join(d['content-type'])
+    
+                #Age is used to determine if the item in cache is fresh or not. So, it for post processing. It does not determin while storing that the object is cacheable or not.
+                #age        = "".join(d['age']).strip()
+                
+                rd.lastModified     = "".join(d['last-modified']).strip()
+                rd.expires  = "".join(d['expires']).strip()
+                rd.pragma       = "".join(d['pragma']).strip()
+            
+            
+                #_______________________________________________________________________
+                cacheControl = ",".join(d['cache-control'])
+                cacheControlArray = cacheControl.split(",")
 
-				for record in cacheControlArray:
-					#CACHE CONTROL DIRECTIVES THOSE ARE CONSIDERED IN THE ALGORITHM
-					if record.find("public")>=0:
-						rd.isPublic = 1
-					if record.find("private")>=0: 
-						rd.isPrivate = 1
-					if record.find("no-store")>=0:
-						rd.isNoStore = 1
-					if record.find("no-cache")>=0:
-						rd.isNoCache = 1
+                for record in cacheControlArray:
+                    #CACHE CONTROL DIRECTIVES THOSE ARE CONSIDERED IN THE ALGORITHM
+                    if record.find("public")>=0:
+                        rd.isPublic = 1
+                    if record.find("private")>=0: 
+                        rd.isPrivate = 1
+                    if record.find("no-store")>=0:
+                        rd.isNoStore = 1
+                    if record.find("no-cache")>=0:
+                        rd.isNoCache = 1
 
-					if record.find("max-age") >= 0:
-						try:
-							rd.maxAge=record.split("=")[1]
-						except:
-							rd.maxAge=-222
+                    if record.find("max-age") >= 0:
+                        try:
+                            rd.maxAge=record.split("=")[1]
+                        except:
+                            rd.maxAge=-222
 
-					if record.find("must-revalidate") >= 0:
-						rd.mustRevalidate = 1
+                    if record.find("must-revalidate") >= 0:
+                        rd.mustRevalidate = 1
 
-					###NOT-CONSIDERED##
-						#no-transform
-						#proxy-revalidate
-						#s-maxage
-						#cache-extension
-					####################
-				#--------------------------------------------------------------------
+                    ###NOT-CONSIDERED##
+                        #no-transform
+                        #proxy-revalidate
+                        #s-maxage
+                        #cache-extension
+                    ####################
+                #--------------------------------------------------------------------
 
-				vary = "".join(d['vary']).strip()
-				if vary.find("*") >= 0:
-					rd.isVaryValidationForced = 1
+                vary = "".join(d['vary']).strip()
+                if vary.find("*") >= 0:
+                    rd.isVaryValidationForced = 1
 
-				sitedetail.addResource(rd)
-				totalObjects += 1
-				if totalObjects >= 1000:
-					break
-				#print rd.url
+                sitedetail.addResource(rd)
+                totalObjects += 1
+                if totalObjects >= 1000:
+                    break
+                #print rd.url
 
-		sitedetail.serialize()					
-		print "PROCESSING " + self.URL.rstrip() + " DONE"
+        sitedetail.serialize()                  
+        print "PROCESSING " + self.URL.rstrip() + " DONE"
